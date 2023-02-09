f246660e3432f3873ac9e4810b3c7c932f79d590
JY-02 <124655720+JY-02@users.noreply.github.com>
2023-02-09 17:12:46 +0900

    shopping

--- src/store/product/productSlice.js
+++ src/store/product/productSlice.js
@@ -5,7 +5,7 @@ export const getProduct = createAsyncThunk(
     "GET_PRODUCT",
     async (keyword) => {
         try{
-            const res =await axios.get(`http://localhost:8000/productList?q=${keyword}`);
+            const res =await axios.get('https://my-json-server.typicode.com/JY-02/shopping/');
             return res.data;
         }catch(err){
             console.log(err)
