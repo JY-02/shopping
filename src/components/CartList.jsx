fcc57d500eb55d9fd9aa0a4b65c2ceba7111a1bd
JY-02 <124655720+JY-02@users.noreply.github.com>
2023-02-10 11:55:36 +0900

    shopping

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -5,7 +5,7 @@ import './CartList.css';
 const CartList = () => {
     const cart = useSelector(state=>state.cart);
     const cartItem = cart.length >= 1 ?  cart.map((item, idx)=>{
-        return <CartItem key={item.id} item={item}  />}) :  <div className="cart-emty">장바구니에 담은 물건이 없습니다.</div>
+        return <CartItem key={item.id} item={item}  />}) :  <div className="cart-emty">장바구니에 추가된 항목 없음</div>
   
     return (
         <div className='cart-list'>
